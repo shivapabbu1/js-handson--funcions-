787a06c52ceb54823e79ce2828ea9c82910fbdda
shivapabbu1 <shivaprasad.pabbu@gmail.com>
2022-12-14 00:45:43 +0530

    js functions

---
+++ main.js
@@ -0,0 +1,61 @@
+//  function (){
+//     console.log()
+//  }
+
+
+// function sum(a,b){
+//     return (a+b);
+
+// }
+// console.log(sum(3,7));
+
+    // hello =()=>{
+    // return("welcome to arrow functions");
+    // }
+
+//  }
+ 
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();
+
+
+ 
+// var x = 21;
+// girl ();
+// console.log(x)
+// function girl() {
+//     console.log(x);
+//     var x = 20;
+// }; ans 21
+
+// var x = 21;
+// a();
+// b();
+
+//   function a() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+//  function b() {
+    
+//     x = 40;
+//    console.log(x);
+// }; ans 20 40
+// function factorial(n) {
+//     if (n == 0){
+
+     
+//     return 1;
+//     }
+//     else{
+
+    
+//     return n * factorial(n - 1);
+//     }
+//   }
+  
